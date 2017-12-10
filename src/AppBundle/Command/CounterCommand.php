89a7dfa0340dfcdaa467ec4c2fbd532f06c2d1f5
Amir Iskander <amir@amiriskander.com>
2017-12-10 15:30:47 +0200

    Testing some progress bar.

--- src/AppBundle/Command/CounterCommand.php
+++ src/AppBundle/Command/CounterCommand.php
@@ -1,45 +1,60 @@
 <?php
 
-// src/AppBundle/Command/CreateUserCommand.php
 namespace AppBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
-class PrinterCommand extends Command
+class CounterCommand extends Command
 {
     protected function configure()
     {
         $this
             // the name of the command (the part after "app/console")
-            ->setName('app:printer')
+            ->setName('app:counter')
 
             // the short description shown while running "php app/console list"
-            ->setDescription('Prints a message.')
+            ->setDescription('Just Counts.')
 
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp('This command allows you to print a message...')
-            ->addArgument('message', InputArgument::REQUIRED, 'The message you would like to print.')
+            ->setHelp('This command allows you to count from zero to a number, showing a progress bar.')
             ->addArgument('count', InputArgument::OPTIONAL, 'The number of times to print.')
-            ;
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // outputs multiple lines to the console (adding "\n" at the end of each line)
         $output->writeln([
-            'Printing Message',
-            '================',
+            'Starting the counter',
+            'Tick Tock Tick Tock',
+            '--------------------',
             '',
         ]);
 
         // outputs a message without adding a "\n" at the end of the line
         $count = $input->getArgument('count') ?? 1;
+
+        $progress = new ProgressBar($output, $count);
+        $progress->start();
+
         for ($i = 0; $i < $count; $i++) {
-            $output->writeln($input->getArgument('message'));
+            // $output->writeln($count);
+            sleep(1);
+            $progress->advance();
         }
+
+        $progress->finish();
+
+        $output->writeln([
+            '',
+            '',
+            "Congratulations! You just wasted $count seconds :D",
+            '',
+        ]);
     }
 }
